e3f28814869ce5946c1062c073cd0c91cc87bae5
Zachacious <back4moore@gmail.com>
2022-08-11 21:16:15 -0500

    scrollbar color

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,11 +1,14 @@
-import { FooterCopyright, Section } from 'astro-boilerplate-components';
+// import { FooterCopyright, Section } from 'astro-boilerplate-components';
 
-import { AppConfig } from '@/utils/AppConfig';
+const year = new Date().getFullYear();
 
 const Footer = () => (
-  <Section>
-    <FooterCopyright site_name={AppConfig.site_name} />
-  </Section>
+  <div class="my-8 flex w-full justify-center opacity-50">
+    © Copyright {year} Zach Moore
+  </div>
+  // <Section>
+  //   <FooterCopyright site_name={AppConfig.site_name} />
+  // </Section>
 );
 
 export { Footer };
