f73d8a7e2c4052eef836fad6f5a3501168699655
Zachacious <back4moore@gmail.com>
2022-08-22 20:49:26 -0500

    work on search

--- src/partials/SearchBtn.tsx
+++ src/partials/SearchBtn.tsx
@@ -1,17 +1,40 @@
+import { useEffect, useState } from 'react';
+
+import IconClose from '@/icons/close';
 import IconSearch from '@/icons/search';
 import debounce from '@/utils/debounce';
 
 const SearchBtn = (props: {
   className: string | undefined;
   onClick: Function;
-}) => (
-  <div
-    className={`${props.className} flex items-center`}
-    onClick={debounce(() => props.onClick(), 100)}
-  >
-    <IconSearch className="icon-sm" />
-    <span className="text-lg">Search</span>
-  </div>
-);
+  // model: boolean;
+}) => {
+  const [model, setModel] = useState(false);
+
+  const onClick = () => {
+    props.onClick();
+    setModel(!model);
+  };
+
+  useEffect(() => {
+    setModel(false);
+  }, []);
+
+  return (
+    <div
+      className={`${props.className} flex items-center`}
+      onClick={debounce(() => onClick(), 100)}
+    >
+      {model ? (
+        <IconClose className="icon-sm" />
+      ) : (
+        <span className="flex items-center">
+          <IconSearch className="icon-sm" />
+          <span className="text-lg">Search</span>
+        </span>
+      )}
+    </div>
+  );
+};
 
 export default SearchBtn;
