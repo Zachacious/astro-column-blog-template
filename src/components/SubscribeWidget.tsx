9068ad165d79d7d006df641bb14af634c92182f8
Zachacious <back4moore@gmail.com>
2022-09-15 19:51:57 -0500

    finished styling on subscribe widget

--- src/components/SubscribeWidget.tsx
+++ src/components/SubscribeWidget.tsx
@@ -8,20 +8,21 @@ const SubscribeWidget = () => {
   };
 
   return (
-    <div className="my-4 flex w-full flex-wrap justify-end rounded-md bg-boomred p-4 text-textdark drop-shadow-[0_5px_5px_rgba(0,0,0,0.3)] dark:bg-boomdark dark:text-primarydark">
+    <div className="my-4 flex w-full flex-wrap justify-end rounded-md  text-primarydark   dark:text-textdark">
       <div className="text-exo w-full text-left text-2xl font-bold">Hey</div>
       <div className="w-full text-left text-sm font-bold">
-        If you're interested in weekly updates, resources and other cool stuff
+        If you're interested in weekly updates, resources and other cool
+        stuff...
       </div>
       <input
         id="searchbox"
-        className="my-2 w-full border-b-2 border-accent bg-transparent text-xl font-bold placeholder:text-accent focus:border-textdark focus:outline-none"
+        className="my-2 w-full border-b-2 border-primarydark bg-transparent text-xl font-bold text-primarydark placeholder:text-primarydark focus:outline-none dark:border-textdark dark:text-textdark dark:placeholder:text-textdark dark:focus:border-textdark"
         type="text"
         value={inputValue}
         onChange={handleInputChange}
         placeholder="email@domain.com"
       />
-      <div className="my-2 cursor-pointer rounded-md bg-primarydark p-2 text-lg text-textdark">
+      <div className="my-2 cursor-pointer rounded-md bg-boom p-2 text-lg font-bold text-primarydark  dark:bg-boomdark">
         Subscribe
       </div>
     </div>
