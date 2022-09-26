d40ed0cc39d2795a50ea31c27871c8a851d6be1c
Zachacious <back4moore@gmail.com>
2022-09-25 23:47:26 -0500

    work on seo and meta tags

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: 'Zach Moore Blog',
-  title: 'Zach Moore Blog',
+  title: 'ZachMoore.dev',
   description: 'Boilerplate built with Astro using React and Tailwind CSS',
   author: 'Zach Moore',
   locale_region: 'en-us',
@@ -13,11 +13,8 @@ export const AppConfig = {
     appleTouchIcon: '/icon.png',
     maskIcon: '/icon.png',
     maskIconColor: '#0B6E4F',
-
     msTileColor: '#0B6E4F',
     themeColor: '#1a2125',
     iosStatusBarColor: '#1a2125',
-
-    rating: 'General',
   },
 };
